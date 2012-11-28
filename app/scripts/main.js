fc616483c2fde1ea230da66b312b73e8ef0eded6
Ryan Neufeld <ryan@neufeldmail.com>
2012-11-28 08:26:16 -0800

    Moved code to app.js Added colors.js - mapping of xterm colors to hex codes. Replaced RGB sliders with forground/background sliders.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,8 +6,79 @@ require.config({
     jquery: 'vendor/jquery.min'
   }
 });
- 
-require(['app'], function(app) {
-  // use app here
-  console.log(app);
+
+require(['colors'], function(colors) {
+    var $element = $('.user');
+
+    function hexFromRGB(r, g, b) {
+        var hex = [
+            r.toString( 16 ),
+            g.toString( 16 ),
+            b.toString( 16 )
+        ];
+        $.each( hex, function( nr, val ) {
+            if ( val.length === 1 ) {
+                hex[ nr ] = "0" + val;
+            }
+        });
+        return hex.join( "" ).toUpperCase();
+    }
+
+    function updateFgColor() {
+        var color = $( "#fg" ).slider( "value" );
+        $element.css( "color", colors[color] ).data('fgcolor', color);
+        console.log(color);
+    }
+
+    function updateBgColor() {
+        var color = $( "#bg" ).slider( "value" );
+        $element.css( "background-color", colors[color] ).data('bgcolor', color);
+        console.log(color);
+    }
+
+
+    function refreshBold(){
+        if($(this).val() === '1')
+        {
+            $element.css('font-weight','bold');
+        }
+        else
+        {
+            $element.css('font-weight','normal');
+        }
+    }
+
+    $(function() {
+        $( "#fg" ).slider({
+            orientation: "horizontal",
+            range: "min",
+            max: 15,
+            min: 0,
+            slide: updateFgColor,
+            change: updateFgColor
+        });
+
+        $( "#bg" ).slider({
+            orientation: "horizontal",
+            range: "min",
+            max: 15,
+            min: 0,
+            slide: updateBgColor,
+            change: updateBgColor
+        });
+
+        $( "#weight" ).find('input').click(refreshBold).end().buttonset();
+
+        $( '#prompt > span, #terminal' ).click(function(e){
+            var currFgColor = 15, currBgColor = 0;
+
+            $element = $(e.target);
+
+            currFgColor = $element.data('fgcolor') || currFgColor;
+            currBgColor = $element.data('bgcolor') || currBgColor;
+
+            $( "#fg" ).slider( "value", currFgColor );
+            $( "#bg" ).slider( "value", currBgColor );
+        });
+    });
 });
